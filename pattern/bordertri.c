874e7bbbc449cc093175eeaa81a5b9325b2f6406
Chandrashekhar Tripathi <tripahtics@outlook.com>
2021-02-04 16:14:41 +0530

    learned to print borders

---
+++ pattern/bordertri.c
@@ -0,0 +1,23 @@
+/*to print
+* 
+* * 
+*   * 
+*     * 
+* * * * *           */
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if (i==j || i==1 || i==5 || j==1)
+            printf("* ");
+            else
+            printf("  ");
+        }
+        printf("\n");
+    }
+}
