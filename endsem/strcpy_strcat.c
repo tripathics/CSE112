25151c0df8ce2173d36852b31181f2eb2873ab37
Chandrashekhar Tripathi <tripahtics@outlook.com>
2021-03-15 13:59:21 +0530

    done strcpy_strcat finally

--- endsem/strcpy_strcat.c
+++ endsem/strcpy_strcat.c
@@ -19,24 +19,29 @@ int main(void)
     printf("Enter a string:\n");
     fgets(src, MAX - 1, stdin);
 
-    //Declaring dest and concat strings
+    //Declaring dest
     string dest = malloc(strlen(src) + 1);
-    string concat = malloc(2 * strlen(src) + 1);
 
     //Copying string from src to dest
     if (src != NULL)
     {
-        if (dest != NULL && concat != NULL)
+        if (dest != NULL)
         {
             strcpy(dest, src);
 
-            // Copying src string to concat
-            strcpy(concat, src);
             //Printing the source string and copied string
             printf("Source string in src:\n%s\n", src);
             printf("Copied string in dest:\n%s\n", dest);
         }
     }
+
+    string concat = malloc(strlen(dest) + strlen(src) + 1);
+    if (concat != NULL)
+    {
+        // Copying src string to concat
+        strcpy(concat, src);
+    }
+    
     
     // Concatenating concat and dest and hence appending to concat
     strcat(concat, dest);
