118238d5463444f60a2c331686c95ebac7f757ba
Chandrashekhar Tripathi <tripathics@outlook.com>
2021-04-10 17:28:03 +0530

    redifination of main in ritesh_old.c

--- ritesh_old.c
+++ ritesh_old.c
@@ -4,24 +4,7 @@ int main()
 {
        int x, i;
        float y, final, z, add=0;
-       printf("Enter the value of x: ")       ;
-       scanf("%d", &x);
-       z = (x-1)/x;
-       for(i=2; i<=7; i++)
-       {
-              y = (1*pow((x-1)/x , i))/2;
-              add = add + y;
-       }
-       final = z + add;
-       printf("Total sum = %f\n", final);
-}
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-       int x, i;
-       float y, final, z, add=0;
-       printf("Enter the value of x: ")       ;
+       printf("Enter the value of x: ");
        scanf("%d", &x);
        z = (x-1)/x;
        for(i=2; i<=7; i++)
