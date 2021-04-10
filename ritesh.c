91fa8aff16f42560373805b0a2e2846f41c9f95f
Chandrashekhar Tripathi <68462214+tripathics@users.noreply.github.com>
2021-04-10 17:40:56 +0530

    Added precision

--- ritesh.c
+++ ritesh.c
@@ -2,12 +2,15 @@
 #include<math.h>
 int main()
 {
-       int i;
+       int i, pres, count;
        float x, y, z, add = 0;
        printf("Enter the value of x: ");
        scanf("%f", &x);
+       printf("How much precise do you want the result to be?\n(1 to 10)");
+       scanf("%d", &pres);
+       count = pres * 5;
        z = (x-1)/x;
-       for(i=1; i < 8; i++)
+       for(i=1; i < count; i++)
        {
               y = (pow(z, i)) / (float)i;
               add = add + y;
